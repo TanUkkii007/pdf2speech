7cf75d865353504d7b5324978d970560e3430feb
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-01-01 21:44:07 +0900

    zero-pad filename to make cat command output in order add output directory option

--- pagenated-files.js
+++ pagenated-files.js
@@ -1,16 +1,32 @@
 const fs = require('fs');
 const Writable = require('stream').Writable;
 
+function zeroPad(number, n) {
+  var numStr = number.toString();
+  while (numStr.length < n) {
+    numStr = '0' + numStr;
+  }
+  return numStr;
+}
 
 class PagenatedFiles extends Writable {
   constructor(options) {
     super();
     this._currentPage = 1;
     this.prefix = options.prefix;
+    this.orderOfPages = options.orderOfPages || 3;
+    this.dirname = options.dirname;
+    this._directoryCreationNeeded = options.dirname && true;
   }
 
   _write(chunk, encoding, callback) {
-    fs.open(`${this.prefix}-${this._currentPage}.mp3`, 'w', (error, fd) => {
+    if (this._directoryCreationNeeded) {
+      if (!fs.existsSync(this.dirname)) {
+        fs.mkdirSync(this.dirname);
+      }
+      this._directoryCreationNeeded = false;
+    }
+    fs.open(`${this.dirname}/${this.prefix}-${zeroPad(this._currentPage, 3)}.mp3`, 'w', (error, fd) => {
       if (error) {
         callback(error);
       } else {
