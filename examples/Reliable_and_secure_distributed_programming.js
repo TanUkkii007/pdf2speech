366301d53ef7850080c0e59759bcdc9264940ac2
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-01-12 23:23:09 +0900

    add “Reliable and secure distributed programming” example

---
+++ examples/Reliable_and_secure_distributed_programming.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const util = require('util');
+const AWS = require('aws-sdk');
+
+require('pdfjs-dist');
+
+AWS.config.update({region: 'us-east-1'});
+
+const PdfTextContentStream = require('../pdf-stream').PdfTextContentStream;
+const PollySpeechSynthesisStream = require('../aws-polly').PollySpeechSynthesisStream;
+const LineConcat = require('../lineconcat').LineConcat;
+const PagenatedFiles = require('../pagenated-files').PagenatedFiles;
+const WordCountLimit = require('../word-count-limit').WordCountLimit;
+const TextTransformFilter = require('../text-transform-filter').TextTransformFilter;
+const ItemFilter = require('../item-filter').ItemFilter;
+const Hyphenation = require('../hyphenation').Hyphenation;
+const InsertAfter = require('../insert-after').InsertAfter;
+const PageBoundarySentencesConcat = require('../page-boundary-sentences-concat').PageBoundarySentencesConcat;
+
+const pdfPath = process.argv[2];
+const data = new Uint8Array(fs.readFileSync(pdfPath));
+
+function insertPredicate(item) {
+  return item.fontName === 'g_d0_f1' &&
+    (item.transform[0] === 11.9552 ||
+     item.transform[0] === 13.9477);
+}
+
+const chapter1 = {
+  startPage: 21,
+  stopPage: 38
+};
+
+const pdfText = new PdfTextContentStream(Object.assign({source: data}, chapter1));
+
+pdfText
+  .pipe(new TextTransformFilter({filterOps: [{index: 5, op: '<', value: 623}, {index: 5, op: '>', value: 51}]}))
+  .pipe(new PageBoundarySentencesConcat())
+  .pipe(new Hyphenation({hyphen: '-'}))
+  .pipe(new InsertAfter({insertCharacter: '.\n', predicate: insertPredicate}))
+  .pipe(new LineConcat())
+  .pipe(process.stdout);
+// pdfText
+//   .pipe(new TextTransformFilter({filterOps: [{index: 5, op: '<', value: 623}, {index: 5, op: '>', value: 51}]}))
+//   .pipe(new PageBoundarySentencesConcat())
+//   .pipe(new Hyphenation({hyphen: '-'}))
+//   .pipe(new InsertAfter({insertCharacter: '.\n', predicate: insertPredicate}))
+//   .pipe(new WordCountLimit({limit: 1500}))
+//   .pipe(new LineConcat())
+//   .pipe(new PollySpeechSynthesisStream({voiceId: 'Joanna'}))
+//   .pipe(new PagenatedFiles({dirname: 'Reliable_and_secure_distributed_programming', prefix: 'ch01'}));
